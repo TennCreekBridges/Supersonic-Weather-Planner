5a75d3b12976ababb2e66f8c8e636904ad5e4bbe
TennCreekBridges <bridgettvalet@gmail.com>
2022-01-30 13:40:30 -0600

    adds storage of favorite cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 // globals
-var searchButtonEl = document.querySelector('#search-button');
+var searchButtonEl = document.querySelector("#search-button");
+var historyArray = document.querySelector("#cities");
+var resetButton = document.querySelector("resetBtn");
 
 // API fetch and function
 var getWeatherApi = function () {
-    var cityName = document.getElementById('search-input').value;
+    var cityName = document.getElementById("search-input").value;
     console.log(cityName);
-    const weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=1f7f67bb2143af57cf3bf01da71a8e1f";
+    const weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=1f7f67bb2143af57cf3bf01da71a8e1f&units=imperial";
     fetch(weatherApi)
     .then(function(response) {
         if (response.ok) {
@@ -14,9 +16,32 @@ var getWeatherApi = function () {
             })
         }
     })
+
+    // save to localStorage
+    var savedCities = JSON.parse(localStorage.getItem("city"));
+    if (savedCities == null) {
+      savedCities = [];
+    }
+    var favCities = cityName;
+    savedCities.push(favCities);
+    localStorage.setItem("city", JSON.stringify(savedCities));
+    console.log(localStorage.getItem("city"));
+    // display searched cities to search history section
+    historyArray.innerHTML = savedCities.join('<br/><hr/>');
 };
 
-// save to localStorage
+var resetFavorites = function () {
+    historyArray.textContent = "";
+    localStorage.clear();
+    console.log("cleared city search history");
+}
 
 // eventListener for Search button
 searchButtonEl.addEventListener("click", getWeatherApi);
+
+resetButtonEl.addEventListener("click", resetFavorites);
+
+
+// empty city-box div and append current weather of searched city
+// $('#city-box').empty();
+// $('#city-box').append(cityName, weatherIcon, weatherType, cityTemp, cityHumidity, cityWindSpeed);
